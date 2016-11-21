5b87b17c5dc231cd992770afce8708eaa2b2ae01
liujunbin <976500133@qq.com>
2016-11-21 17:24:16 +0800

    source chang

--- source/txtconfig.js
+++ source/txtconfig.js
@@ -1,12 +1,1 @@
- $(document).ready(function(){
 
-   var options = {
-     editor: document.getElementById('mdcontent'), // {DOM Element} [required]
-     class: 'txt', // {String} class of the editor,
-     debug: true, // {Boolean} false by default
-     textarea: '<textarea name="content"></textarea>', // fallback for old browsers
-     linksInNewWindow: true  // open hyperlinks in a new windows/tab
-  }
-  var editor = new Txt(options);
-
-});
